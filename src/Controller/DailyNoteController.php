bf90b7519aa4f2b7c04b1a12d644fb182023547a
BerkeAnna <berke.anna0803@gmail.com>
2023-04-15 18:33:40 +0200

    dailyNote header

--- src/Controller/DailyNoteController.php
+++ src/Controller/DailyNoteController.php
@@ -4,15 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class IndexController extends AbstractController
+class DailyNoteController extends AbstractController
 {
+
     public function index()
     {
         $pageTitle = "Bujo";
-        return $this->render('base.html.twig', [
-            'page_title' => $pageTitle,
-        ]);
+        return $this->render('dailyNote.html.twig', [
+            'page_title' => $pageTitle]);
     }
+
 }
+
 
 ?>
